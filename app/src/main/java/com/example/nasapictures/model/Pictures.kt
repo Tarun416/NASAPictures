a9827e1eb499710682f1a29180bb6afedb95176a
tarun416 <trn.khanna5@gmail.com>
2020-12-02 01:14:44 +0530

    first commit  : created package for showing listing and details , also added model data class

---
+++ app/src/main/java/com/example/nasapictures/model/Pictures.kt
@@ -0,0 +1,24 @@
+package com.example.nasapictures.model
+import com.google.gson.annotations.SerializedName
+
+
+class Pictures : ArrayList<PicturesItem>()
+
+data class PicturesItem(
+    @SerializedName("copyright")
+    val copyright: String?,
+    @SerializedName("date")
+    val date: String?,
+    @SerializedName("explanation")
+    val explanation: String?,
+    @SerializedName("hdurl")
+    val hdurl: String?,
+    @SerializedName("media_type")
+    val mediaType: String?,
+    @SerializedName("service_version")
+    val serviceVersion: String?,
+    @SerializedName("title")
+    val title: String?,
+    @SerializedName("url")
+    val url: String?
+)
