b9d06399a989269e17beb6a79f5ea6d748dbfda2
tarun416 <trn.khanna5@gmail.com>
2020-12-05 20:01:48 +0530

    remove toolbar and change status bar color to black

--- app/src/main/java/com/example/nasapictures/MainActivity.kt
+++ app/src/main/java/com/example/nasapictures/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        supportActionBar!!.hide()
         supportFragmentManager.beginTransaction()
             .replace(R.id.content, ListFragment.newInstance())
             .commit()
